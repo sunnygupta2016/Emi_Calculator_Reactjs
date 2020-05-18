3be63e568607c4ba3286c559be7c105f27770561
Sunny Gupta <gupta.sunny2016@gmail.com>
2020-05-18 15:18:39 +0545

    update

--- src/Components/frontend/frontpage.js
+++ src/Components/frontend/frontpage.js
@@ -2,6 +2,8 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const monthlyEmi = (loanAmount, interest, tenure) => {
   let rateOfInt = interest/100/12;
@@ -62,11 +64,10 @@ class Frontpage extends React.Component{
 
     return(
       <React.Fragment>
-      <h3>EMI Calculator </h3>
-      <h6>Developed By Sunny Gupta </h6>
-       <div class="container-fluid">
-         <div class="row">
-           <div class="col-sm-6">
+       <Container p={0} className={'emi-calci'} maxWidth="md">
+          <h1 align={"center"} m={0}>Emi_Calculator  (Developed By Sunny Gupta)</h1>
+          <Grid container spacing={1}>
+            <Grid container xs={12} sm={4} md={4} spacing={4} item>
                <form class="form">
                     
                     <TextField id="outlined-basic"label="Enter_LoanAmount" color="secondary" name="loanAmount" onChange={this.inputHandleChange}/><br/>
@@ -76,18 +77,21 @@ class Frontpage extends React.Component{
                      <div class="operation-btns">
                   <Button variant="contained" color="primary" onClick={this.calculateEmi}> Calculate </Button>
                       </div>
-               </form>  
+               </form> 
+               </Grid>
+               </Grid>
+               </Container> 
+            
+            <div class="flex">
+              
+                <h4><span>LoanAmount(Rs):{loanAmount}</span></h4>
+                <h4><span>Monthly_Emi(Rs):{monthlyEmiAmount}</span></h4>
+                <h4><span>Interest_Amount Payable(Rs):{interestAmt}</span></h4>
+                <h4><span>Total_Amount_Payable(Rs):{totalAmt}</span></h4>
+             
             </div>
-            <div class="col-sm-6">
-              <div id="result">
-                <h2><span class="badge badge-success">LoanAmount(Rs):{loanAmount}</span></h2>
-                <h2><span class="badge badge-success">Monthly_Emi(Rs):{monthlyEmiAmount}</span></h2>
-                <h2><span class="badge badge-success">Interest_Amount Payable(Rs):{interestAmt}</span></h2>
-                <h2><span class="badge badge-success">Total_Amount_Payable(Rs):{totalAmt}</span></h2>
-             </div>
-            </div>
-         </div>
-       </div>
+        
+       
       </React.Fragment>
      )
   }
