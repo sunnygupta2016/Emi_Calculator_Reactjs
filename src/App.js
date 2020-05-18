95745ffb779a838524e9c92e81cbc327383bbcfe
Sunny Gupta <gupta.sunny2016@gmail.com>
2020-05-18 13:12:11 +0545

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Frontpage from './Components/frontend/frontpage';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <Router>
-     <Route exact path={'/'} component={Frontpage} />
-     </Router>
+    
+     <Frontpage />
+ 
   );
 }
 
