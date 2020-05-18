30a4a4c6497bb8610f4f35e88c1f2c50eede5ed9
Sunny Gupta <gupta.sunny2016@gmail.com>
2020-05-18 13:18:57 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Frontpage from './Components/frontend/frontpage';
+
 
 /*ReactDOM.render(
   <React.StrictMode>
